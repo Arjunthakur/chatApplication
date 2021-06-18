89ab5dcea5b2d6f459922c1a2f10171741eeafe9
unknown <arjunthakur2002@gmail.com>
2021-06-19 00:21:49 +0530

    entire app

--- components/Loading.js
+++ components/Loading.js
@@ -4,6 +4,7 @@ function Loading() {
     return (
         <center style={{ display:"grid", placeItems: "center", height:"100vh" }}>
             <div>
+                <div className="lds-hourglass"></div>
                 <h1>Loading...</h1>
             </div>
         </center>
